e4e8d5f488da8ad4b16b8d618befa37294260ac4
ttucker <tracytuckeremail@gmail.com>
2021-11-15 17:52:17 -0600

    adding icons to header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Header from './components/Header';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     // Block Element Modifier naming convention
     <div className="app">
       {/* Header */}
+      <Header />
       {/* Sidebar */}
       {/* React-Router -> Chat screen */}
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import '../css/Header.css'
-import { Avatar } from '@mui/icons-material'
+import { Avatar } from '@mui/material'
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import SearchIcon from '@mui/icons-material/Search';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 function Header() {
     return (
@@ -10,18 +12,23 @@ function Header() {
                 {/* Avatar for logged-in user */}
                 < Avatar
                     className="header__avater"
-                    alt={user?.displayName}
-                    src={user?.photoURL}
+                    alt="Tracy Tucker"
+                    src=''
+                    // alt={user?.displayName}
+                    // src={user?.photoURL}
                 />
                 {/* Time icon */}
                 <AccessTimeIcon />
             </div>
             <div className="header__search">
                 {/* Search icon */}
+                <SearchIcon />
                 {/* Input */}
+                <input placeholder="Search your team" />
             </div>
             <div classname="header__right">
                 {/* Help icon */}
+                <HelpOutlineIcon />
             </div>
         </div>
     )
