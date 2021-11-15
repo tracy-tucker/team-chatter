a9d2f8444a9f6272798d59e029cb9a37932c22b0
ttucker <tracytuckeremail@gmail.com>
2021-11-15 12:57:02 -0600

    Adding building notes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // BEM naming convention
+    <div className="app">
+      {/* Header */}
+      {/* Sidebar */}
+      {/* React-Router -> Chat screen */}
     </div>
   );
 }
