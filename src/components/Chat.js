5f9cca56afbea1446ee84f2cab913a86615c93cc
ttucker <tracytuckeremail@gmail.com>
2021-11-19 09:11:28 -0600

    Adding full header styling to Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import '../css/Chat.css'
 import { useParams } from 'react-router-dom'
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import InfoIcon from '@mui/icons-material/Info';
 
 function Chat() {
     console.log('Where am I?')
     const { roomId } = useParams()
     return (
         <div className="chat">
-            <h2>You are in the {roomId} room</h2>
+            <div className="chat__header">
+                <div className="chat__headerLeft">
+                    <h4 className="chat__channelName">
+                        {/* Pull name from DB */}
+                        <strong>#General</strong>
+                        <StarBorderIcon />
+                    </h4>
+                </div>
+                <div className="chat__headerRight">
+                    <p>
+                        <InfoIcon /> Details
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
