b7bc631b1c4c22b4dac80d59792d6dcbd741efb8
ttucker <tracytuckeremail@gmail.com>
2021-11-19 09:32:37 -0600

    Working on redirecting clicks to new chat rooms

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import '../css/SidebarOption.css'
+import { useNavigate } from 'react-router-dom'
 
-function SidebarOption({Icon, title}) {
+function SidebarOption({Icon, title, id, addChannelOption}) {
+    // useNavigate replaces useHistory in react-router-dom v6
+    // When you click on a link/go back/go forward
+    // push the next page into history: AKA forcing a redirect
+    const navigate = useNavigate();
+
+    const selectChannel = () => {
+        if (id) {
+            if (id) {
+                navigate.push(`/room/${id}`);
+            } else {
+                navigate.push(title);
+            }
+        }
+    }
+
+    const addChannel = () => {
+        //
+    }
+
     return (
-        <div className="sidebarOption">
+        <div className="sidebarOption"
+            onClick={addChannelOption ? addChannel : selectChannel}>
             {/* only render Icon if it exists */}
             {Icon && <Icon className="sidebarOption__icon" />}
             {/* If no icon, then it represents a channel */}
