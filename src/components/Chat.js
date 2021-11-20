15812cb49a862fd58a7886ee88ba8d0c380b4159
ttucker <tracytuckeremail@gmail.com>
2021-11-20 07:52:31 -0600

    Adding messages for each chat room

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/Chat.css'
 import { useParams } from 'react-router-dom'
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import InfoIcon from '@mui/icons-material/Info';
+import db from '../firebase/config';
 
 function Chat() {
-    console.log('Where am I?')
+    
     const { roomId } = useParams()
+    const [roomDetails, setRoomDetails] = useState(null)
+    const [roomMessages, setRoomMessages] = useState(null)
+
+    useEffect(() => {
+        if (roomId) {
+            db.collection('rooms').doc(roomId)
+            .onSnapshot((snapshot) => setRoomDetails(snapshot.data()))
+        }
+        db.collection('rooms')
+        .doc(roomId)
+        .collection('messages')
+        .orderBy('timestamp', 'asc')
+        .onSnapshot((snapshot) =>
+            setRoomMessages(
+                snapshot.docs.map(doc => doc.data())
+        ))
+
+    }, [roomId])
+    console.log(roomMessages)
+
     return (
         <div className="chat">
             <div className="chat__header">
                 <div className="chat__headerLeft">
                     <h4 className="chat__channelName">
                         {/* Pull name from DB */}
-                        <strong>#General</strong>
+                        <strong>#{roomDetails?.name}</strong>
                         <StarBorderIcon />
                     </h4>
                 </div>
