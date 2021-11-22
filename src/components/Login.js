b18a8737ec9f1dd216418db76d5d6edb7988261a
ttucker <tracytuckeremail@gmail.com>
2021-11-22 13:27:23 -0600

    Adding Google auth to project

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import '../css/Login.css'
 import { Button } from "@mui/material"
+import { auth, provider } from '../firebase/config'
+
 
 function Login() {
+    const signIn = (e) => {
+        auth
+        .signInWithPopup(provider)
+        .then((result) => {
+            console.log(result)
+        })
+        .catch((error) => {
+            alert(error.message)
+        })
+    }
     return (
         <div className="login">
             <div className="login__container">
                 <img src="https://res.cloudinary.com/tracyr/image/upload/v1483126281/teamPlayer_lgtva8.png" alt="login icon" />
                 <h1>Sign in to Team Chatter HQ</h1>
                 <p>team-chatter.channel.com</p>
-                <Button>Sign in with Google</Button>
+                <Button onClick={signIn}>Sign in with Google</Button>
             </div>
         </div>
     )
 }
 
 export default Login
+
+// 3:17:19
