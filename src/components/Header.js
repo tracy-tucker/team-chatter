0c8ad0e5703f08989dced1fb31ac6006e78f3772
ttucker <tracytuckeremail@gmail.com>
2021-12-30 13:31:04 -0600

    adding user info to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,19 @@ import { Avatar } from '@mui/material'
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import SearchIcon from '@mui/icons-material/Search';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import { useStateValue } from "../context/StateProvider"
+
 
 function Header() {
+    const [{user}] = useStateValue()
     return (
         <div className="header">
             <div className="header__left">
                 {/* Avatar for logged-in user */}
                 < Avatar
                     className="header__avater"
-                    alt="Tracy Tucker"
-                    src=''
+                    alt={user?.displayName}
+                    src={user?.photoURL}
                     // alt={user?.displayName}
                     // src={user?.photoURL}
                 />
