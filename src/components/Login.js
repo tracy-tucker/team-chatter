3a43ffed87766eb52fb7ae3fbd830632cee07294
ttucker <tracytuckeremail@gmail.com>
2021-12-30 13:25:26 -0600

    passing in user data to firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,21 @@ import React from 'react'
 import '../css/Login.css'
 import { Button } from "@mui/material"
 import { auth, provider } from '../firebase/config'
+import { useStateValue } from "../context/StateProvider"
+import { actionTypes } from "../reducer/reducer"
 
 
 function Login() {
+    const [state, dispatch] = useStateValue()
     const signIn = (e) => {
         auth
         .signInWithPopup(provider)
         .then((result) => {
             console.log(result)
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: result.user
+            })
         })
         .catch((error) => {
             alert(error.message)
